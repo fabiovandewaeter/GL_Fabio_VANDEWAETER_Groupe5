bd85171e9d2cfee405b8305ea8399cf1ddd2ddb3
fabio vandewaeter <fabio.vandewaeter.etu@univ-lille.fr>
2024-01-09 17:29:05 +0100

    3.1 premier test - création

---
+++ GL_Fabio_VANDEWAETER/src/main/java/Compte.java
@@ -0,0 +1,18 @@
+
+public class Compte {
+	private int credit;
+	private int debit;
+	
+	public Compte() {
+		this.credit = 0;
+		this.debit = 0;
+	}
+	
+	public int getCredit() {
+		return this.credit;
+	}
+	
+	public int getDebit() {
+		return this.debit;
+	}
+}
--- GL_Fabio_VANDEWAETER/src/test/java/CompteTest.java
+++ GL_Fabio_VANDEWAETER/src/test/java/CompteTest.java
@@ -3,10 +3,10 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-class CompteTest {
+public class CompteTest {
 	
 	@Test
-	void newAccountsStartWithCreditAndDebitOf0() {
+	public void newAccountsStartWithCreditAndDebitOf0() {
 		Compte c = new Compte();
 		assertEquals(0, c.getCredit());
 		assertEquals(0, c.getDebit());
