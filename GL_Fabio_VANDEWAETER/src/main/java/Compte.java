5f6ab4672e159904af4167a4112f0c5a1346f8e9
fabio vandewaeter <fabio.vandewaeter.etu@univ-lille.fr>
2024-01-16 17:09:28 +0100

    refactoring by removing useless tests and add Amélioration 2

--- GL_Fabio_VANDEWAETER/src/main/java/Compte.java
+++ GL_Fabio_VANDEWAETER/src/main/java/Compte.java
@@ -1,34 +1,73 @@
 
 public class Compte {
-	private int credit;
-	private int debit;
+	private final static int ARRAY_SIZE = 5;
+	private double[] credits;
+	private double[] debits;
+	private int creditIndex;
+	private int debitIndex;
 	
 	public Compte() {
-		this.credit = 0;
-		this.debit = 0;
+		this.credits = new double[ARRAY_SIZE];
+		this.debits = new double[ARRAY_SIZE];
+		this.creditIndex = 0;
+		this.debitIndex = 0;
 	}
 	
-	public int getCredit() {
-		return this.credit;
+	public double getCredit() {
+		int res = 0;
+		for (double n : this.credits) {
+			res += n;
+		}
+		return res;
 	}
 	
-	public int getDebit() {
-		return this.debit;
+	public double getDebit() {
+		int res = 0;
+		for (double n : this.debits) {
+			res += n;
+		}
+		return res;
 	}
 	
-	public void creditAccount(int amount) {
+	public double[] getCreditsArray() {
+		return this.credits;
+	}
+	
+	public double[] getDebitsArray() {
+		return this.debits;
+	}
+	
+	public void creditAccount(double amount) {
 		if (amount >= 0) {
-			this.credit += amount;
+			if (this.creditIndex < this.credits.length) {
+				this.credits[this.creditIndex] = amount;
+				this.creditIndex++;
+			}
+			else {
+				double sum = this.getCredit();
+				this.credits = new double[ARRAY_SIZE];
+				this.credits[0] = sum;
+				this.creditIndex = 0;
+			}
 		}
 	}
 	
-	public void debitAccount(int amount) {
+	public void debitAccount(double amount) {
 		if (amount >= 0) {
-			this.debit += amount;
+			if (this.debitIndex < this.debits.length) {
+				this.debits[this.debitIndex] = amount;
+				this.debitIndex++;
+			}
+			else {
+				double sum = this.getDebit();
+				this.debits = new double[ARRAY_SIZE];
+				this.debits[0] = sum;
+				this.debitIndex = 0;
+			}
 		}
 	}
 	
-	public int getBalance() {
-		return this.credit-this.debit;
+	public double getBalance() {
+		return this.getCredit()-this.getDebit();
 	}
 }
