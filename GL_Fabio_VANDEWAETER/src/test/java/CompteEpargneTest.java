ed8e83d76a04d7fca8546bf7f30eb485fbc994ed
Vandewaeter Fabio <fabio.vandewaeter.etu@univ-lille.fr>
2024-01-21 19:52:00 +0100

    4.3) Calculer et créditer les intérets | add failed tests for echeance

--- GL_Fabio_VANDEWAETER/src/test/java/CompteEpargneTest.java
+++ GL_Fabio_VANDEWAETER/src/test/java/CompteEpargneTest.java
@@ -12,9 +12,10 @@ public class CompteEpargneTest extends AbstractCompteTest<CompteEpargne>{
 	}
 	
 	@Test
-	public void checkCreditAndDebitAreAt0() {
+	public void checkCreditAndDebitAndInterestAreAt0() {
 		assertEquals(0, this.c.getCredit());
 		assertEquals(0, this.c.getDebit());
+		assertEquals(0, this.c.getInterest());
 	}
 	
 	/*
@@ -39,5 +40,14 @@ public class CompteEpargneTest extends AbstractCompteTest<CompteEpargne>{
 		assertEquals(INTEREST_RATE, this.c.getInterestRate());
 		this.c.creditAccount(1000);
 		assertEquals((INTEREST_RATE/100)*this.c.getCredit(), this.c.calculInterest());
+	}
+	
+	@Test
+	public void echeanceAddInterest() {
+		this.c.creditAccount(100);
+		assertEquals(0, this.c.getInterest());
+		double tempoBalance = this.c.getBalance();
+		this.c.echeance();
+		assertEquals(tempoBalance+this.c.calculInterest(), this.c.getBalance());
 	}
 }
