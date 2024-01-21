b0666511da3c07b9f4c36b0dd2d1502db78ee8f8
Vandewaeter Fabio <fabio.vandewaeter.etu@univ-lille.fr>
2024-01-21 21:52:19 +0100

    5.2) Ouverture de comptse et de comptse épargne | tests OK

--- GL_Fabio_VANDEWAETER/src/main/java/Banque.java
+++ GL_Fabio_VANDEWAETER/src/main/java/Banque.java
@@ -11,4 +11,20 @@ public class Banque {
 	public List<Compte> getComptes() {
 		return this.comptes;
 	}
+
+	public int numberOfComptes() {
+		return this.comptes.size();
+	}
+
+	public Compte createCompte() {
+		Compte newAccount = new Compte();
+		this.comptes.add(newAccount);
+		return newAccount;
+	}
+	
+	public CompteEpargne createCompteEpargne() {
+		CompteEpargne newAccount = new CompteEpargne(2.0);
+		this.comptes.add(newAccount);
+		return newAccount;
+	}
 }
--- GL_Fabio_VANDEWAETER/src/test/java/BanqueTest.java
+++ GL_Fabio_VANDEWAETER/src/test/java/BanqueTest.java
@@ -1,3 +1,4 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.BeforeEach;
