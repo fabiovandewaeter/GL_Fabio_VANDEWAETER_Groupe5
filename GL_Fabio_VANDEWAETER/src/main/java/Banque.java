31c09895176c3bf3698d6473a94398b5a3ec0409
Vandewaeter Fabio <fabio.vandewaeter.etu@univ-lille.fr>
2024-01-21 21:39:22 +0100

    5.1) Ouverture de banque | test OK

--- GL_Fabio_VANDEWAETER/src/main/java/Banque.java
+++ GL_Fabio_VANDEWAETER/src/main/java/Banque.java
@@ -1,4 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
 
 public class Banque {
+	private List<Compte> comptes;
 	
+	public Banque() {
+		this.comptes = new ArrayList<>();
+	}
+
+	public List<Compte> getComptes() {
+		return this.comptes;
+	}
 }
--- GL_Fabio_VANDEWAETER/src/test/java/BanqueTest.java
+++ GL_Fabio_VANDEWAETER/src/test/java/BanqueTest.java
@@ -1,3 +1,5 @@
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
